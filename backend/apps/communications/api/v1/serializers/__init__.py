ff4ecb5685edd730fe10c4f73d7e4eba74a2740b
Flavio Marques <Flavionegocios2004@gmail.com>
2026-07-17 11:36:33 -0300

    refactor(communications): organiza exports de serializers

---
+++ backend/apps/communications/api/v1/serializers/__init__.py
@@ -0,0 +1,31 @@
+from .channels import CommunicationChannelConfigSerializer
+from .communications import (
+    CommunicationAttemptSerializer,
+    CommunicationAutomationRunSerializer,
+    CommunicationAutomationSerializer,
+    CommunicationCreateSerializer,
+    CommunicationDetailSerializer,
+    CommunicationDraftUpdateSerializer,
+    CommunicationListSerializer,
+    CommunicationPreferenceSerializer,
+    CommunicationRecipientSerializer,
+    CommunicationTemplateSerializer,
+    InAppNotificationSerializer,
+    NotificationPreferenceSerializer,
+)
+
+__all__ = [
+    "CommunicationAttemptSerializer",
+    "CommunicationAutomationRunSerializer",
+    "CommunicationAutomationSerializer",
+    "CommunicationChannelConfigSerializer",
+    "CommunicationCreateSerializer",
+    "CommunicationDetailSerializer",
+    "CommunicationDraftUpdateSerializer",
+    "CommunicationListSerializer",
+    "CommunicationPreferenceSerializer",
+    "CommunicationRecipientSerializer",
+    "CommunicationTemplateSerializer",
+    "InAppNotificationSerializer",
+    "NotificationPreferenceSerializer",
+]
